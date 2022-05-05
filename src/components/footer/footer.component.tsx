6b7b46d0be7bead8ae5d4bd325e9588e2a593bda
EdilsonMello <93235712+EdilsonMello@users.noreply.github.com>
2022-05-05 07:07:55 -0300

    FEAT(Front-End) feature/create-footer

---
+++ src/components/footer/footer.component.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Container } from '../../styles/pages/footer.styles'
+import Mail from '../../assets/mail.svg'
+
+const Footer: React.FC = () => {
+  return (
+    <Container>
+      <div className="baner">
+        <h1>Get started today!</h1>
+        <button>
+          <p>PICK A PLAN</p>
+        </button>
+      </div>
+      <div className="content">
+        <div className="logo">
+          <Mail/>
+          <p>NinjaMail</p>
+        </div>
+        <div className="options">
+          <div className="sections">
+            <p>Features</p>
+            <p>Pricing</p>
+            <p>Services</p>
+            <p>Partners</p>
+          </div>
+          <div className="sections">
+            <p>About Us</p>
+            <p>Tutorials</p>
+            <p>Resources</p>
+            <p>Help Center</p>
+            <p>Templates</p>
+            <p>Case Studies</p>
+          </div>
+          <div className="sections">
+            <p>Medium</p>
+            <p>Twitter</p>
+            <p>Facebook</p>
+            <p>Instagram</p>
+            <p>LinkedIn</p>
+          </div>
+          <div className="sections">
+            <p>Contact Us</p>
+            <p>Slack</p>
+            <p>Jobs</p>
+          </div>
+        </div>
+      </div>
+      <span/>
+      <div className="terms">
+        <h1>NinjaMail is a sample project for Quest AI. © 2019 Quest AI, All rights reserved.</h1>
+        <p>Terms & Conditions</p>
+        <p>Privacy Policy</p>
+      </div>
+    </Container>
+  );
+}
+
+export default Footer;
