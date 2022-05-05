358dbca368c4264ae4709dd8126df9f3098904a9
EdilsonMello <edilson_mello.oficial@outlook.com>
2022-05-04 22:14:43 -0300

    create navbar component

--- src/components/navbar/navbar.component.tsx
+++ src/components/navbar/navbar.component.tsx
@@ -1,13 +1,46 @@
-import React from 'react';
+import React from 'react'
 import Mail from '../../assets/mail.svg'
-import { Container } from '../../styles/pages/navbar.style';
+import { Container } from '../../styles/pages/navbar.style'
+import Link from 'next/link'
 // import { Container } from './styles';
 
 const Navbar: React.FC = () => {
   return (
     <Container>
-      <Mail/>
-      <p>NinjaMail</p>
+      <div className="content">
+        <Mail/>
+        <p>NinjaMail</p>
+        <div className="links">
+          <div>
+            <Link href=''>
+              <a>FEATURES</a>
+            </Link>
+            <span/>
+          </div>
+          <div>
+            <Link href=''>
+              <a>PRICING</a>
+            </Link>
+            <span/>
+          </div>
+          <div>
+            <Link href=''>
+              <a>SERVICES</a>
+            </Link>
+            <span/>
+          </div>
+          <div>
+            <Link href=''>
+              <a>PARTNERS</a>
+            </Link>
+            <span/>
+          </div>
+        </div>
+        <button>
+          <p>SIGN UP FREE</p>
+        </button>
+      </div>
+
     </Container>
   )
 }
